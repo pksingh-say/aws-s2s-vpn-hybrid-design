b722e852aaa91df32543bdf7659d643f71618496
pksingh-say <pawansingh.gpg87@gmail.com>
2026-01-14 17:29:59 +0530

    Update route-tables.tf

--- terraform/route-tables.tf
+++ terraform/route-tables.tf
@@ -10,3 +10,8 @@ resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
 }
+resource "aws_route" "to_onprem" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "172.16.0.0/16"
+  gateway_id             = aws_vpn_gateway.vgw.id
+}
