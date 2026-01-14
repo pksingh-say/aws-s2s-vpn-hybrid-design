e92816388f00ac39602c16df3930de61b4f43032
pksingh-say <pawansingh.gpg87@gmail.com>
2026-01-14 17:26:40 +0530

    Route Table-VPN Routing Logic

---
+++ terraform/route-tables.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
